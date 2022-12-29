35719dc7d723f31a463ed509caedd65c94eadf45
Dianitsya <dianitasyanandamasta@gmail.com>
2022-12-29 17:00:56 +0700

    add variabel di class Node

--- BinarySearchTree_011/Program.cs
+++ BinarySearchTree_011/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree_011
 {
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+    }
     internal class Program
     {
         static void Main(string[] args)
